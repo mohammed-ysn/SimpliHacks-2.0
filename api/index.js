8f92aa5993cf2cd2631601c0c7575ddc81e8cd74
Mohammed Yaseen <mohammedysn15@gmail.com>
2022-06-25 22:56:30 +0100

    Add endpoint to get sea data

--- api/index.js
+++ api/index.js
@@ -6,9 +6,20 @@ const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.send('Hello from the server!'));
 
-app.get('/test', async (req, res) => {
-    // Placeholder data
-    const response = await fetch('https://api.github.com/users/github');
+// /oceandata/{latitude},{longitude},{depth in metres}
+app.get('/oceandata/:latlngdpth', async (req, res) => {
+    const latlngdpth = req.params.latlngdpth.split(',');
+
+    const url = new URL('https://ocean.amentum.io/rtofs');
+    url.searchParams.append('latitude', latlngdpth[0]);
+    url.searchParams.append('longitude', latlngdpth[1]);
+    url.searchParams.append('depth', latlngdpth[2]);
+
+    const response = await fetch(url.href, {
+        headers: {
+            'API-Key': 'sTB9wQkdj2Jvhpt7dTD8tyOOW6Y4LOoz'
+        }
+    });
     const data = await response.json();
     res.json(data);
 });
