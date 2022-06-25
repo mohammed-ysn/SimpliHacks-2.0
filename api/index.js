5a14025e56a858e39ea67126ab037cf7e7e49be5
Mohammed Yaseen <mohammedysn15@gmail.com>
2022-06-25 20:03:07 +0100

    Use node-fetch to receive json

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
+import express from 'express';
+import fetch from 'node-fetch';
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.send('Hello from the server!'));
+
+app.get('/test', async (req, res) => {
+    // Placeholder data
+    const response = await fetch('https://api.github.com/users/github');
+    const data = await response.json();
+    res.json(data);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
